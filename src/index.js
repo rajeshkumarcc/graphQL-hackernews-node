0d566733a13aec4fd4c9c6126cb928b7258ccd57
Rajesh Kumar <rajesh.kumar@causecode.com>
2018-04-30 16:57:53 +0530

    Separated graphql schema. Added full crud operation with in-memory object

--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
 const {GraphQLServer} = require('graphql-yoga');
 
-// The typeDefs constant defines your GraphQL schema, and info here is the root field of the schema
-const typeDefs = `
-type Query {
-    info: String!
-    feed: [Link!]!
-}
-
-type Link {
-    id: ID!
-    description: String!
-    url: String!
-}
-`
 let links = [{
     id: 'link-0',
     url: 'www.howtographql.com',
     description: 'Fullstack tutorial for GraphQL'
 }]
+
+let idCount = links.length
+
+const removeElement = (array, removeId) => {
+    return array.filter(e => e.id !== removeId);
+};
 
 // The resolvers object is the actual implementation of the GraphQL schema.
 // Notice how its structure is identical to the structure of the type definition inside typeDefs: Query.info.
@@ -25,20 +18,39 @@ const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
         feed: () => links,
+        link: (root, args) => links.find(link => link.id === args.id),
     },
 
-    // Note: If we don't provide resolver functions here, it will still work; because the implementation of the Link resolvers is trivial and will be taken care by GraphQL.
-    // Link: {
-    //     id: (root) => root.id,
-    //     description: (root) => root.description,
-    //     url: (root) => root.url
-    // }
+    Mutation: {
+        post: (root, args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url,
+            }
+            links.push(link)
+            return link
+        },
+
+        updateLink: (root, args) => {
+            const index = links.findIndex(link => link.id === args.id);
+            const updatedLink = {id, url, description} = args;
+            links[index] = updatedLink;
+            return links[index];
+        },
+
+        deleteLink: (root, args) => {
+            const deletedLink = links.find(link => link.id === args.id)
+            links = removeElement(links, args.id);
+            return deletedLink;
+        }
+    },
 }
 
 // 3. The schema and resolvers are bundled and passed to the GraphQLServer
 // This tells the server what API operations are accepted and how they should be resolved.
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers
 });
 
